d0796eb42c52bf74c6f8ee95c002d31468497e29
otot <rgotot@icloud.com>
2022-05-04 23:31:06 +0900

    feat: 基本機能の追加

--- app/controllers/wallet_controller.rb
+++ app/controllers/wallet_controller.rb
@@ -1,4 +1,15 @@
 class WalletController < ApplicationController
   def show
+    @wallet = Wallet.find(params[:id])
+    logger.debug("スタート")
+    sleep(10)
+    logger.debug("終了")
+    if @wallet.is_count_used?
+      flash.now[:count] = '使用済みです'
+    else
+      @wallet.use
+      flash.now[:count] = '使用しました'
+    end
+    @wallet.save
   end
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,2 +1,17 @@
 class Wallet < ApplicationRecord
+
+    def is_count_used?
+        if self.count == 1
+            return true
+        else
+            return false
+        end
+    end
+
+    def use
+        logger.debug("スタート")
+        # sleep(10)
+        logger.debug("終了")
+        self.count =1
+    end
 end
